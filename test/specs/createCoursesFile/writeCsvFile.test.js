b31dce39769c83a482984a6b931e5bd7a685fc03
Emil Stenberg <emilsten@kth.se>
2017-02-15 12:14:06 +0100

    added a test for writing the headers

--- test/specs/createCoursesFile/writeCsvFile.test.js
+++ test/specs/createCoursesFile/writeCsvFile.test.js
@@ -21,6 +21,16 @@ test('should call buildCanvasCourseObjects to rebuild the input to a format that
 
 test('should write a line with the headers', t => {
   t.plan(1)
+  createCoursesFile.__set__('buildCanvasCourseObjects', arg => [])
+  const courseRounds = [[{ courseCode: 'AL0001'}]]
+
+  writeCsvFile(courseRounds, 'fileName').then(() => {
+    t.ok(csvFile.writeLine.calledWith(['course_id', 'short_name', 'long_name', 'start_date', 'account_id', 'status'], 'fileName'))
+  }).catch(e => console.error(e))
+})
+
+test('should write a line with the headers', t => {
+  t.plan(1)
   createCoursesFile.__set__('buildCanvasCourseObjects', arg => [{courseId: '1', shortName: 'shortName', longName: 'longName', startDate: 'startDate', sisAccountId: 'sisAccountId', status: 'status'}])
   const courseRounds = [[{ courseCode: 'AL0001'}]]
 
