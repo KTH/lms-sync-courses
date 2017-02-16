e7909317e4ac2fccda5a3ee597934d47e43c2eb6
Emil Stenberg <emilsten@kth.se>
2017-02-16 11:03:22 +0100

    in the integration test: verify the content of the file

--- test/integration/createCoursesFile.test.js
+++ test/integration/createCoursesFile.test.js
@@ -2,13 +2,21 @@ var test = require('tape')
 fs = require('fs-extra')
 
 const createCoursesFile = require('../../createCoursesFile.js')
-test('should create the file with correct name', t => {
-  t.plan(1)
+test('should create the file with correct name, headers, and a line including a sisCourseId', t => {
+  t.plan(2)
   fs.removeSync('csv')
+  const fileName = 'csv/courses-2017:1-3.csv'
 
   // Choose autumn, but a period on the spring to get a smaller amount of courses
   createCoursesFile({term: '1', year: '2017', period: '3'})
   .then(() => {
-    t.ok(fs.existsSync('csv/courses-2017:1-3.csv'))
+    t.ok(fs.existsSync(fileName))
+
+    const fileContent = fs.readFileSync(fileName, 'utf-8')
+    const [headers,line2] = fileContent.split('\n')
+    t.equal(headers, 'course_id,short_name,long_name,start_date,account_id,status')
+
+    const [courseId] = line2.split(',')
+    t.ok(courseId !== 'undefined' && courseId.length === 6)
   })
 })
--- test/specs/createCoursesFile/buildCanvasCourseObjects/createLongName.test.js
+++ test/specs/createCoursesFile/buildCanvasCourseObjects/createLongName.test.js
@@ -3,21 +3,6 @@ const rewire = require('rewire')
 const createCoursesFile = rewire('../../../../createCoursesFile.js')
 const createLongName = createCoursesFile.__get__('createLongName')
 
-/*
-function _courseTerm (courseRoundObj) {
-  const startTerm = courseRoundObj.courseRound.$.startTerm
-  const startTermCanonical = startTerm[4] === '1' ? 'VT' : 'HT'
-  return `${startTermCanonical}${startTerm.substring(2, 4)}`
-}
-
-function _createTitle (courseObj, courseRoundObj, xmlLang) {
-  const courseTitle = courseObj.course.title.find(title => title.$['xml:lang'] === xmlLang)._
-  const term = _courseTerm(courseRoundObj)
-  return `${term}-${courseRoundObj.courseRound.$.roundId} ${courseTitle}`
-}
-
-ID1003VT172,ID1003,ID1003 TCOMK VT17-2 Project IT,2017-03-20T12:36:29.612Z,ICT - Imported course rounds,active
-*/
 test('should use the swedish title', t => {
   t.plan(1)
   const courseRound =
