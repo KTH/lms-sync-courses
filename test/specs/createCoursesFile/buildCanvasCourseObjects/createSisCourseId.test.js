bc2d45aa76417703bf9c0bd3ea14de2e7eb72648
Emil Stenberg <emilsten@kth.se>
2017-02-16 10:32:43 +0100

    created failing test for sisCourseId

--- test/specs/createCoursesFile/buildCanvasCourseObjects/createSisCourseId.test.js
+++ test/specs/createCoursesFile/buildCanvasCourseObjects/createSisCourseId.test.js
@@ -1,5 +1,16 @@
 const test = require('tape')
-test('should do something', t => {
-  t.plan(1)
-  t.equal(1, 0)
+const rewire = require('rewire')
+const createCoursesFile = rewire('../../../../createCoursesFile.js')
+const createSisCourseId = createCoursesFile.__get__('createSisCourseId')
+
+// let sis_course_id = `${course_code}${_courseTerm(courseRoundObj)}${courseRoundObj.courseRound.$.roundId}`
+test.only('should take a 2d array as input, and return a 1d array', t => {
+  const result = createSisCourseId({
+    courseCode: 'AL2140',
+    startTerm: '20171',
+    roundId: '1'
+  })
+
+  t.deepEqual(result, 'AL2140VT171')
+  t.end()
 })
--- test/specs/createCoursesFile/buildCanvasCourseObjects/flatten.test.js
+++ test/specs/createCoursesFile/buildCanvasCourseObjects/flatten.test.js
@@ -3,7 +3,7 @@ const rewire = require('rewire')
 const createCoursesFile = rewire('../../../../createCoursesFile.js')
 const flatten = createCoursesFile.__get__('flatten')
 
-test.only('should take a 2d array as input, and return a 1d array', t => {
+test('should take a 2d array as input, and return a 1d array', t => {
   const input = [[{1:2}], [{3:4}]]
   const result = flatten(input)
 
