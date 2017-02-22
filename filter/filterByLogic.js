9f8c8c31a65374aff3d569005d20f8ce8f087736
Emil Stenberg <esandin@gmail.com>
2017-02-22 11:01:53 +0100

    added a test for filterByLogic

--- filter/filterByLogic.js
+++ filter/filterByLogic.js
@@ -1,12 +1,13 @@
-const {uniqWith, isEqual} = require('lodash');
+const {uniqWith, isEqual} = require('lodash')
 const filterYellowCourses = require('./filterYellowCourses')
 const filterOneToOne = require('./filterOneToOne')
 
 function filterByLogic (groupedCourses) {
   const oneToOneCourses = filterOneToOne(groupedCourses)
   const yellowCourses = filterYellowCourses(groupedCourses)
+
   const allCourses = [...oneToOneCourses, ...yellowCourses]
-  return uniqWith(allCourses, isEqual);
+  return uniqWith(allCourses, isEqual)
 }
 
 module.exports = filterByLogic
