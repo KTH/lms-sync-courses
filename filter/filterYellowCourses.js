bf149e655c6a050dec8880419abee70c46540887
Elena Rakhimova <elenara@kth.se>
2017-02-24 10:26:10 +0100

    Function filter By Turoting language and test are added

--- filter/filterYellowCourses.js
+++ filter/filterYellowCourses.js
@@ -3,14 +3,26 @@ const {countBy} = require('lodash')
 function includeRoundsIfDifferentStartWeekForEachRound (courseRounds) {
   const roundsPerWeek = countBy(courseRounds, 'startWeek')
   let allRoundsHaveDifferentStartWeek = true
-  console.log('roundsPerWeek', roundsPerWeek)
   for (let key in roundsPerWeek) {
     if (roundsPerWeek[key] > 1) {
       allRoundsHaveDifferentStartWeek = false
     }
   }
-  console.log('allRoundsHaveDifferentStartWeek', allRoundsHaveDifferentStartWeek)
   return allRoundsHaveDifferentStartWeek ? courseRounds : []
+}
+
+function filterByTutoringLanguage (courseRounds) {
+  const roundsByLang = countBy(courseRounds, 'tutoringLanguage')
+  let isDiffLang = true
+  for (let key in roundsByLang) {
+    if (roundsByLang[key] > 1) {
+      isDiffLang = false
+    }
+    if (key === 'undefined') {
+      isDiffLang = false
+    }
+  }
+  return isDiffLang ? courseRounds : []
 }
 
 module.exports = function (groupedCourses) {
