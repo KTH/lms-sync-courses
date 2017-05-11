832576c678de2915b995f70721eae8917868ed56
Emil Stenberg <emilsten@kth.se>
2017-05-11 12:35:19 +0200

    added courses to ignore list

--- RemoveTheseCourses.js
+++ RemoveTheseCourses.js
@@ -1,6 +1,15 @@
-// arry should include course codes like: [
+// array should include course codes like: [
 //  "LT1018",
 //  "AL1300",
 //  "SK2340"
 //  ]
-module.exports.courseList = []
+module.exports.courseList = [
+  'A31H2A',
+  'A31P2D',
+  'A42D14',
+  'EF111X',
+  'EJ2230',
+  'EP242X',
+  'MM4000',
+  'MT1011'
+]
