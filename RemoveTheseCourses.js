57be1290e7669f040c1d42967110d3e2efca67d6
Emil Stenberg <emilsten@kth.se>
2017-08-22 14:51:07 +0200

    empty the skipped courses

--- RemoveTheseCourses.js
+++ RemoveTheseCourses.js
@@ -3,13 +3,4 @@
 //  "AL1300",
 //  "SK2340"
 //  ]
-module.exports.courseList = [
-  'A31H2A',
-  'A31P2D',
-  'A42D14',
-  'EF111X',
-  'EJ2230',
-  'EP242X',
-  'MM4000',
-  'MT1011'
-]
+module.exports.courseList = []
