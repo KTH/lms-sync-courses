1415efab6b4a0ed6f36fed8791d3a93e498eb760
Elena Rakhimova <elenara@kth.se>
2018-03-21 10:43:31 +0100

    started reading createcoursefile

--- config/serverSettings.js
+++ config/serverSettings.js
@@ -2,5 +2,5 @@ require('dotenv').config()
 
 module.exports = {
   port: 3000,
-  foo: process.env.FOO
+  koppsBaseUrl: process.env.koppsBaseUrl,
 }
--- run-periodically.js
+++ run-periodically.js
@@ -1,12 +1,19 @@
 const logger = require('./server/logger')
+const createCoursesFile = require('./old/createCoursesFile')
+const serverSettings = require('./config/serverSettings')
+const year = '2018'
+const term = '1'
+const period = '3'
+
 
 async function syncCourses(){
-  logger.info('hejj')
+  createCoursesFile.koppsBaseUrl = serverSettings.koppsBaseUrl
+  await createCoursesFile.createCoursesFile({term, year, period})
 }
 
 module.exports = {
   start () {
     syncCourses()
-    setInterval(syncCourses, 1000)
+    //setInterval(syncCourses, 1000)
   }
 }
