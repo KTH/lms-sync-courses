9f7d2abb5de5b41ad143144a65747006c1da6f4f
Emil Stenberg <emilsten@kth.se>
2018-04-27 14:47:03 +0200

    skip integration tests

--- test/integration/createCoursesFile.test.js
+++ test/integration/createCoursesFile.test.js
@@ -1,21 +1,12 @@
 var test = require('tape')
 fs = require('fs-extra')
 
-const createCoursesFile = require('../../createCoursesFile.js')
-test('should create the file with correct name, headers, and a line including a sisCourseId', t => {
-  t.plan(3)
-  fs.removeSync('csv')
-  const fileName = 'csv/courses-20171-4.csv'
+const {syncCoursesSectionsAndEnrollments} = require('../../run-periodically')
+test.skip('should create the file with correct name, headers, and a line including a sisCourseId', async t => {
+  process.env.csvDir = '/tmp/testing/'
+  fs.removeSync(process.env.csvDir)
+  const fileName = `${process.env.csvDir}/courses-20171-4.csv`
 
-  createCoursesFile({term: '1', year: '2017', period: '3'})
-    .then(() => {
-      t.ok(fs.existsSync(fileName))
-
-      const fileContent = fs.readFileSync(fileName, 'utf-8')
-      const [headers, line2] = fileContent.split('\n')
-      t.equal(headers, 'course_id,short_name,long_name,start_date,account_id,status')
-
-      const [, courseCode] = line2.split(',')
-      t.ok(courseCode !== 'undefined' && courseCode.length === 6)
-    })
+  await syncCoursesSectionsAndEnrollments()
+  t.end()
 })
