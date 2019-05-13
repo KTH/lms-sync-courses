6eb6109fe4b8bbe5951f14cd940b670a13274e53
kstarzecki <k.starzecki@gmail.com>
2019-05-13 12:39:50 +0200

    added integrationId

--- test/unit/createCoursesFile/buildCanvasCourseObjects/buildCanvasCourseObjects.test.js
+++ test/unit/createCoursesFile/buildCanvasCourseObjects/buildCanvasCourseObjects.test.js
@@ -9,6 +9,7 @@ test('should create course object containing attributes needed for creating the 
   utils.__set__('createSisCourseId', () => 'abc123')
   utils.__set__('createLongName', () => 'Långt namn')
   utils.__set__('calcStartDate', () => aDate)
+  utils.__set__('integrationId', () => 'd1ff3r3n-71nt-3gr4-710n-1dt3571ng123')
 
   const courseRound = {
     schoolCode: 'ABE',
@@ -30,6 +31,7 @@ test('should create course object containing attributes needed for creating the 
     longName: 'Långt namn',
     startDate: `${aDate}T06:00:00Z`,
     sisAccountId: 'ABE - Imported course rounds',
+    integrationId: 'd1ff3r3n-71nt-3gr4-710n-1dt3571ng123',
     status: 'active'
   }
 
