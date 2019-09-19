a2a124a64bb96f9e142ce815c2fab465ac88b044
Carlos Saito <carlos@saito.style>
2019-09-19 10:49:01 +0200

    Catch also unhandled promise rejections

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const logger = require('./server/logger')
 const runPeriodically = require('./run-periodically')
 const packageFile = require('./package.json')
 
-// catches uncaught exceptions
-process.on('uncaughtException', (err, origin) => {
-  logger.error('APPLICATION EXIT - uncaught exception in ', packageFile.name)
-  logger.error(`Uncaught Exception, origin (${origin})`, { err })
+process.on('uncaughtException', err => {
+  // In case of unknown Error, just log and crash the app!
+  logger.fatal(err, 'Uncaught Exception thrown')
   process.exit(1)
+})
+
+process.on('unhandledRejection', (reason, p) => {
+  // Pass "unhandledRejection" to the "uncaughtException" handler above
+  throw reason
 })
 
 app.use('/api/lms-sync-courses/', require('./server/systemroutes'))
