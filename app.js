47e434e420528c4663b250ec8d4df20fb52fdf11
Peter Delmeby <peter.delmeby@gmail.com>
2019-09-19 10:38:35 +0200

    Added uncaughtException handler.

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ require('dotenv').config()
 const app = require('kth-node-server')
 const logger = require('./server/logger')
 const runPeriodically = require('./run-periodically')
+const packageFile = require('./package.json')
+
+// catches uncaught exceptions
+process.on('uncaughtException', (err, origin) => {
+  logger.error('APPLICATION EXIT - uncaught exception in ', packageFile.name)
+  logger.error(`Uncaught Exception, origin (${origin})`, { err })
+  process.exit(1)
+})
+
 app.use('/api/lms-sync-courses/', require('./server/systemroutes'))
 app.start({
   port: 3000,
